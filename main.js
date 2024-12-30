bb09b68ed301e8516867f525f657f795f5fa65b0
DevRosses <devrosses@gmail.com.ar>
2024-12-30 17:22:25 -0300

    Se agrego botones flotantes hacia carrito y hero

--- main.js
+++ main.js
@@ -1,10 +1,18 @@
+/*--->> agrega funcionalidad a los botones <<---*/
+
 let btnOpenMenu = document.getElementById("open-menu");
 
 let btnCloseMenu = document.getElementById("close-menu");
 
+let btnOpenCar = document.getElementById("open-car");
+
+let btnCloseCar = document.getElementById("close-car");
+
 let menu = document.getElementById("mobile-menu");
 
-let links = document.getElementsByClassName("links")
+let MenuCar = document.getElementById("mobile-car");
+
+let links = document.getElementsByClassName("links");
 
 btnOpenMenu.addEventListener("click", () => {
   menu.classList.remove("disabled");
@@ -14,6 +22,17 @@ btnCloseMenu.addEventListener("click", () => {
   menu.classList.add("disabled");
 });
 
+btnOpenCar.addEventListener("click", () => {
+  MenuCar.classList.remove("disabled");
+});
+
+btnCloseCar.addEventListener("click", () => {
+  MenuCar.classList.add("disabled");
+});
+
 links.addEventListener("click", () => {
   menu.classList.add("disabled");
 });
+
+/*--->> Solicitamos lista de productos <<---*/
+
